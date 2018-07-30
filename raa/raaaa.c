3ef9af6c03dc3e30edf58461fe4472d1b9fa28b3
Ragulranjith <36723105+Ragulranjith@users.noreply.github.com>
2018-07-30 16:33:10 +0530

    Create raaaa.c

---
+++ raa/raaaa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char a[50];
+    printf("enter the day:");
+    scanf("%s",&a);
+        if(a[0]=='s' || a[0]=='s')
+        {
+            printf("\nyes");
+        }
+        else
+        {
+            printf("\nno");
+        }
+    return 0;
+}
